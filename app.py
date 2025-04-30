f2110e93864c8e1027a47dc0e8b45a0b7ed61ed5
pedro558 <pedroafonso109@gmail.com>
2025-04-30 13:26:10 -0300

    :sparkles: feat: add POST method

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-  return "Hello, World!"
+tasks = []
+task_id_control = 1
 
-@app.route('/about')
-def about():
-  return "This is the about page."
+@app.route('/tasks', methods=['POST'])
+def create_task():
+  global task_id_control
+  data = request.get_json()
+  new_task = Task(
+    id=task_id_control,
+    title=data['title'],
+    description=data.get('description', '')
+  )
+  task_id_control += 1
+  tasks.append(new_task)
+  print(tasks)
+  return jsonify({"message": "Task created successfully"})
+
+
+
+
+
 
 if __name__ == '__main__':
   app.run(debug=True)

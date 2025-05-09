45b6e096d1afc85569c4d5d18eb33b406fefdd67
pedro558 <pedroafonso109@gmail.com>
2025-05-09 11:51:29 -0300

    :test_tube: test: add 'update' and 'delete' tests

--- tests.py
+++ tests.py
@@ -29,3 +29,36 @@ def test_get_task():
     response = requests.get(f"{BASE_URL}/tasks/{task_id}")
     assert response.status_code == 200
     assert task_id == response.json()['id']
+
+
+def test_update_task():
+  if tasks:
+    task_id = tasks[0]
+    payload = {
+        "completed": True,
+        "description": "Nova descrição",
+        "title": "Título atualizado"
+    }
+    response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+    response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+
+    #Nova requisição a tarefa específica
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert response_json["title"] == payload["title"]
+    assert response_json["description"] == payload["description"]
+    assert response_json["completed"] == payload["completed"]
+
+def test_delete_task():
+  if tasks:
+    task_id = tasks[0]
+    response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 404
+
+
